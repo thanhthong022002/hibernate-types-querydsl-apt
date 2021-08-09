9bc9e72ddd35b6883d7fbcea7f31087bdf548125
thong.nguyen <thanhthong022002@gmail.com>
2021-08-09 15:13:27 +0700

    Bump to 10.0.1

--- release-xo.bat
+++ release-xo.bat
@@ -2,4 +2,4 @@ set VERSION=%1
 set BRANCH="xomad-%VERSION%"
 
 mvn versions:set -DgenerateBackupPoms=false "-DnewVersion=%VERSION%" && ^
-mvn -pl "querydsl-ext-api,querydsl-ext-impl,querydsl-ext-apt" -am clean install -DskipTests
+mvn -pl "querydsl-ext-api,querydsl-ext-impl,querydsl-ext-apt" -am clean deploy -DskipTests
