1a02751f687809113debc5432a1e6296978fb20a
thong.nguyen <thanhthong022002@gmail.com>
2023-11-30 15:02:03 +0700

    master Map date database function.

--- querydsl-ext-impl/src/main/java/com/pallasathenagroup/querydsl/CommonFunctionInitializer.java
+++ querydsl-ext-impl/src/main/java/com/pallasathenagroup/querydsl/CommonFunctionInitializer.java
@@ -4,6 +4,7 @@ import org.hibernate.boot.MetadataBuilder;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.spi.MetadataBuilderInitializer;
 import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.type.DateType;
 import org.hibernate.type.StringType;
 
 public class CommonFunctionInitializer implements MetadataBuilderInitializer {
@@ -11,6 +12,7 @@ public class CommonFunctionInitializer implements MetadataBuilderInitializer {
     @Override
     public void contribute(MetadataBuilder metadataBuilder, StandardServiceRegistry standardServiceRegistry) {
         metadataBuilder.applySqlFunction("AS_TEXT", new SQLFunctionTemplate(StringType.INSTANCE, "?1::TEXT"));
+        metadataBuilder.applySqlFunction("date", new SQLFunctionTemplate(DateType.INSTANCE, "date(?1)"));
     }
 
 }
